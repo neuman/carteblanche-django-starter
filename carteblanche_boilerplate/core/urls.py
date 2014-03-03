9e14d708e89e0216466d85775aae478c83674b76
Eric Neuman <soloptimus@gmail.com>
2014-03-03 01:49:35 -0500

    split verbs out into their own file

--- carteblanche_boilerplate/core/urls.py
+++ carteblanche_boilerplate/core/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 import core.views as cv
 
 urlpatterns = patterns('',
-    url(r'users/create/$', cv.UserCreateView.as_view(), name='user_ceate'),
+    url(r'users/create/$', cv.UserCreateView.as_view(), name='user_create'),
     url(r'users/login/$', cv.UserLoginView.as_view(), name='user_login'),
     url(r'sprockets/create/$', cv.SprocketCreateView.as_view(), name='sprocket_create'),
     url(r'sprockets/(?P<pk>\d+)/$', cv.SprocketDetailView.as_view(), name='sprocket_detail'),
